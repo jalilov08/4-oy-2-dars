e3b8f03089999f2d0146e0ae119a450f1fc55241
jalilov08 <jalilovnozimjon054@gmail.com>
2023-11-27 15:11:57 +0500

    Homework finished

---
+++ index.js
@@ -0,0 +1,79 @@
+/** 1-misol
+ *      let a = +prompt("Birinchi insonni yoshini kriting.");
+        let b = +prompt("Ikkinchi insonni yoshini kriting.");
+
+         if ( a > b ) {
+         console.log(a ** 2);
+         } 
+          else {
+         console.log(b ** 2);
+         }
+ */
+
+/** 2-misol
+ *       let a = +prompt("Birinchi insonni yoshini kiriting.");
+         let b = +prompt("Ikkinchi insonni yoshini kiriting.");
+         let c = +prompt("Uchinchi insonni yoshini kiriting.");
+
+         if ( a > b ) {
+         console.log(a);
+         }
+          else {
+         console.log(b);
+         }
+         if ( b < c) {
+         console.log(c);
+         }
+ */ 
+
+ /**  3-misol
+  *     let number = +prompt("Son kiriting");
+
+         if (number % 2 == 0) {
+         console.log("Juft son");
+         }
+          else {
+         console.log("Toq son");
+         }
+  */
+
+/**  4-misol
+ *       let son = +prompt("Son kiriting men uni 5ga karraligini qaniqlab beraman");
+
+         if (son / 5 == 0) {
+         console.log("5ga karrali");
+         } 
+          else {
+         console.log("5ga karrali emas");
+         }
+ */
+
+/**  7-misol
+ *      let son = +prompt("Son kiriting men uni bir xonali sondan katta yoki kichikligini aniqlab beraman");
+         if (son > 9) {
+         console.log(true);
+         }
+          else {
+         console.log(false);
+         }
+ */
+
+/**  8-misol
+ *      let number = +prompt("Son kiriting men uni juft yoki toq ekanligini aniqlab beraman");
+         number = number % 10;
+         if (number % 2 == 0) {
+         console.log(true);
+         }
+          else {
+         console.log(false);
+         }
+*/
+
+/** 9-misol
+ *      let number = +prompt("Son kiriting");
+
+         if (number < 1000) {
+          number = number / 100;
+          console.log(number);
+         }
+ */
